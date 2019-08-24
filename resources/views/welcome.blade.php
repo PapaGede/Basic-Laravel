5c2cefaf15f9fcedf9e064b28d676c3ba8ec29ea
PapaGede <naqhouamy@gmail.com>
2019-08-24 20:46:02 +0300

    Edited some stuff

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
   </div>
 
   <div class="row">
-    <div class="col-md-8">
+    <div class="col-md-9">
         <div class="post">
           <h3>Post Title</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi tempora asperiores dolorum, commodi similique dicta maiores maxime ratione laudantium nemo in expedita dolore, rem illum facere quae exercitationem beatae eum.</p>
@@ -40,6 +40,17 @@
     </div>
     <div class="col-md-3 col-md-offset-1">
       <h2>Sidebar</h2>
+      .<div class="jumbotron jumbotron-fluid">
+        <div class="container-fluid">
+          <h1 class="display-3">Lorem</h1>
+          <p class="lead">Jumbo helper text</p>
+          <hr class="my-2">
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est numquam provident, voluptatem asperiores quidem consequatur, consectetur, tenetur quis et molestias corporis blanditiis adipisci quia obcaecati architecto possimus suscipit! Hic, aliquam!</p>
+          <p class="lead">
+            <a class="btn btn-primary btn-lg" href="#" role="button">Jumbo action name</a>
+          </p>
+        </div>
+      </div>
     </div>
   </div>
 @endsection
