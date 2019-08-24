d730e2e3d9ffa1f7a61b6dfa5c027724bcdfbc2a
PapaGede <naqhouamy@gmail.com>
2019-08-24 17:37:16 +0300

    Created a post controller

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ Route::get('contact', 'PagesController@getcontact');
 Route::get('about','PagesController@getAbout');
 
 Route::get('/','PagesController@getIndex');
+
+Route::resource('posts','PostController');
