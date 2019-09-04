87fe2724fc00effca1dff36ede7f0f5dcddaed6b
PapaGede <naqhouamy@gmail.com>
2019-09-04 17:51:14 +0300

    Added the show template

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,5 +2,27 @@
 @section('title','| View Post')
 
 @section('content')
-    <p class="lead">This is the blog post</p>
+<div class="row">
+    <div class="col-md-12">
+        <div class="jumbotron">
+            <div style="text-align:center" class="post">
+                <h3>{{$post->title}}</h3>
+                <p>{{$post->body}}</p>
+                <small style="text-align:left" class="text-muted">Created at:{{date('M j, Y h:ia',strtotime($post->created_at))}}</small>
+                <div class="row">
+                        <div class="col-sm-6">
+                            {{Html::linkRoute('posts.edit','Edit',array($post->id),array('class'=>'btn btn-primary btn-block'))}}
+                        </div>
+                        <div class="col-sm-6">
+                             {{Form::open(['route'=>['posts.destroy',$post->id],'method'=>'DELETE'])}}
+                             {{Form::submit('Delete',array('class'=>'btn btn-danger btn-block'))}}
+                             {{Form::close()}}
+                            </div>   
+                </div>
+                     
+                </div>
+        </div><hr>
+
+    </div>
+</div>
 @endsection
