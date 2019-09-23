d87b4ce932d21416dd1e0c35f3341d482340b5c8
PapaGede <naqhouamy@gmail.com>
2019-09-23 23:19:55 +0300

    All routes for the blog

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,16 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//Authentication
+Auth::routes();
+Route::get('auth/login','Auth\LoginController@getLogin');
+Route::post('auth/login','Auth\@postLogin');
+Route::get('auth/logout','Auth\LoginController@getLogout');
+
+//Registration
+Route::get('auth/register','Auth\LoginController@getRegister');
+Route::post('auth/register','Auth\LoginController@postRegister');
+
 Route::get('contact', 'PagesController@getcontact');
 
 Route::get('about','PagesController@getAbout');
@@ -19,3 +29,11 @@ Route::get('/','PagesController@getIndex');
 Route::resource('posts','PostController');
 
 date_default_timezone_set('GMT');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
