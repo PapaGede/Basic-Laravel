01bff11bb462be44394255de318be4fb8acb4273
PapaGede <naqhouamy@gmail.com>
2019-10-08 01:50:55 +0300

    Blog with laravel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'hey') }}</title>
+    <title>{{ config('app.name', 'PapaGede') }}</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -23,9 +23,14 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'hey') }}
-                </a>
+                <div class="row">
+                    <div class="col-md-6">
+                        <a class="navbar-brand" href="{{ url('/') }}">
+                            <img src="images/icons8_DOT_48px.png" height="25px">
+                            {{ config('app.name', 'PapaGede') }}
+                        </a>
+                    </div>
+                </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
